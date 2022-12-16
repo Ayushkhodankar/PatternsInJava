1cec4584c30fb415925cd61eda3afc9c80ac0d05
Ayush Khodankar <ayushkhodankar01@gmail.com>
2022-12-16 23:17:24 +0530

    Update LeftTriangleStar.java

--- src/LeftTriangleStar.java
+++ src/LeftTriangleStar.java
@@ -1,22 +1,17 @@
 
 public class LeftTriangleStar {
-	 public static void printStars(int n) 
-	    { 
-	        int i, j;  
-	        for(i=0; i<n; i++)
-	            {           
-	                System.out.print(" ");
-	            } 
-	            for(j=0; j<=i; j++)
-	            {       
-	                System.out.print("* ");
-	            }           
-	            System.out.println(); 
+    public static void main(String args[]) 
+	  { 
+	     int n = 5; 
+	     int i, j;  
+	     for(i=0; i<n; i++)
+	        {           
+	           System.out.print(" ");
 	        } 
-	    
-	    public static void main(String args[]) 
-	    { 
-	        int n = 5; 
-	        printStars(n); 
+	      for(j=0; j<=i; j++)
+	        {       
+	           System.out.print("* ");
+	        }           
+	           System.out.println();  
 	    } 
 }
