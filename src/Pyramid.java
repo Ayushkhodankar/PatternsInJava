bb4dde31a5e6c53074d2de77cae584b309bd1603
Ayush Khodankar <ayushkhodankar01@gmail.com>
2022-12-16 18:47:32 +0530

    initial commit

---
+++ src/Pyramid.java
@@ -0,0 +1,23 @@
+
+public class Pyramid {
+	public static void pyramidPattern(int n) 
+    { int i; 
+        for (i=0; i<n; i++)
+            { 
+                System.out.print(" ");
+            }  
+            for (int j=0; j<=i; j++ ) 
+            { 
+                System.out.print("* "); 
+            } 
+  
+            System.out.println();
+        } 
+     
+  
+    public static void main(String args[]) //driver function
+    { 
+        int n = 5; 
+        pyramidPattern(n); 
+    } 
+}
