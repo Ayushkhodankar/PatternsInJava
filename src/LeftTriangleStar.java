471442bd6893665fc03bcf211893aaad65d2eb08
Ayush Khodankar <ayushkhodankar01@gmail.com>
2022-12-16 23:21:01 +0530

    initial commit

--- src/LeftTriangleStar.java
+++ src/LeftTriangleStar.java
@@ -1,6 +1,6 @@
 
 public class LeftTriangleStar {
-	 public static void printStars(int n) 
+	/* public static void printStars(int n) 
 	    { 
 	        int i, j;  
 	        for(i=0; i<n; i++)
@@ -13,10 +13,21 @@ public class LeftTriangleStar {
 	            }           
 	            System.out.println(); 
 	        } 
-	    
+	    */
 	    public static void main(String args[]) 
 	    { 
 	        int n = 5; 
-	        printStars(n); 
+	        for (int i = 1; i <= n ; i++) {
+	        	for (int j = 1; j <= n - i; j++) {
+	        	System.out.print(" ");
+	        	}
+	        	for (int k = 1; k <= i; k++) {
+	        	System.out.print("*");
+	        	}
+	        	System.out.println("");
+	        	}
+
+
+	        
 	    } 
 }
