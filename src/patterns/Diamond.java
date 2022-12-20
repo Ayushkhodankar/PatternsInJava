ec237631aa7e12922fd22284466d1b182307c65b
Ayush Khodankar <ayushkhodankar01@gmail.com>
2022-12-20 13:17:19 +0530

    initial commit

--- src/patterns/Diamond.java
+++ src/patterns/Diamond.java
@@ -25,15 +25,15 @@ public class Diamond {
 		space = 1;
 		for (j = 1; j<= n - 1; j++)
 		{
-		for (i = 1; i<= space; i++)
-		{
-		System.out.print(" ");
-		}
+			for (i = 1; i<= space; i++)
+			{
+				System.out.print(" ");
+			}
 		space++;
-		for (i = 1; i<= 2 * (n - j) - 1; i++)
-		{
-		System.out.print("*");
-		}
+			for (i = 1; i<= 2 * (n - j) - 1; i++)
+			{
+				System.out.print("*");
+			}
 		System.out.println("");
 		}
 	}
