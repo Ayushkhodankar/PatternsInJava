db6bfde7cd87dff750839b50cb1e072dde98afbd
Ayush Khodankar <ayushkhodankar01@gmail.com>
2022-12-29 14:33:06 +0530

    initial commit

--- src/patterns/DownwardTriangleStar.java
+++ src/patterns/DownwardTriangleStar.java
@@ -12,10 +12,10 @@ public class DownwardTriangleStar {
 	 
 	    for (int i= rows-1; i>=0 ; i--)
 	    {
-	    for (int j=0; j<=i; j++)
-	    {
-	    System.out.print("*" + " ");
-	    }
+	    	for (int j=0; j<=i; j++)
+	    	{
+	    		System.out.print("*" + " ");
+	 		}
 	    System.out.println();
 	    }
 	    sc.close();
